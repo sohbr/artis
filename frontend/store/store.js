e52ae22ab4f1bdccc2a21cd05448ba6b02d483b2
Jimmy Li <jimmyli921@gmail.com>
2017-10-09 20:11:32 -0700

    Add Register Frontend View

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,16 +1,20 @@
 import thunk from 'redux-thunk';
 import {createStore, compose, applyMiddleware} from 'redux';
-// import {AsyncStorage} from 'react-native';
-// import {persistStore, autoRehydrate} from 'redux-persist';
+import {AsyncStorage} from 'react-native';
+import {persistStore, autoRehydrate} from 'redux-persist';
 import rootReducer from '../reducers/root_reducer';
 
 var defaultState = {};
 
 export const configureStore = (initialState = defaultState) => {
-  const store = createStore(rootReducer, initialState, //compose(
-    applyMiddleware(thunk)
-    // autoRehydrate()
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(thunk),
+      autoRehydrate()
+    )
   );
-  // persistStore(store, {storage: AsyncStorage});
+  persistStore(store, {storage: AsyncStorage});
   return store;
 };
