0c5879c9f7edf38b9ecf50427bd82e5fcddb635f
Jimmy Li <jimmyli921@gmail.com>
2017-10-09 20:26:04 -0700

    fix

--- db/migrate/20171010025315_add_columns_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :email, :string
-    add_column :users, :avatar_url, :string
-
-    drop_table :doomis
-  end
-end
--- frontend/reducers/entities_reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import doomisReducer from './doomisReducer';
-
-const entitiesReducer = combineReducers({
-  doomis: doomisReducer
-});
-
-export default entitiesReducer;
