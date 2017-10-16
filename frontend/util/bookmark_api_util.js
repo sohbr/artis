57675dddb72c1575d8e62ec2fb2dabb95fd3803d
Jimmy Li <jimmyli921@gmail.com>
2017-10-16 06:59:09 -0700

    update api urls

--- frontend/util/bookmark_api_util.js
+++ frontend/util/bookmark_api_util.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = "http://localhost:3000";
+// const url = "http://localhost:3000";
+const url = "https://hire-artis.herokuapp.com";
 
 export const postBookmark = (userId, postId) => (
   axios({
